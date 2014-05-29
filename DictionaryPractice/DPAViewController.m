ac7d95fb8b388ee00630d2aa387b234cb6593045
Jon Fox <foxjon123@gmail.com>
2014-05-29 11:03:18 -0400

    Dictionary practice

--- DictionaryPractice/DPAViewController.m
+++ DictionaryPractice/DPAViewController.m
@@ -14,9 +14,24 @@
 
 @implementation DPAViewController
 {
-    NSDictionary * rewardSequenceArray;
-    NSDictionary * twinkleTwinkleArray;
-    NSDictionary * maryHadALittleLambArray;
+    NSArray * colors;
+    NSDictionary * colorDictionary1;
+    NSDictionary * colorDictionary2;
+
+    NSDictionary * faces;
+    
+    int chosenColorIndex;
+    int chosenFaceIndex;
+}
+
+- (NSDictionary *)returnChosenFace
+{
+    return colors[chosenColorIndex][@"faces"][chosenFaceIndex];  //use singleton to grab these
+}
+
+-(void)addFace:(NSDictionary *)face WithColor:(int)colorIndex
+{
+    [colors[colorIndex][@"faces"] addObject:face];
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -24,7 +39,6 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        
     }
     return self;
 }
@@ -32,49 +46,157 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
-    NSArray * songlist = @[@"rewardSequenceArray",
-                           @{
-                               @"notes":@[@7, @1, @2, @4, @7, @0],
-                               @"tempo":@[@100, @400, @500, @600, @700, @800]
-                            },
-                          @"twinkleTwinkleArray",
-                           @{
-                               @"notes":@[@0, @0, @4, @4, @5, @5, @4, @3, @3, @2, @2, @1, @1, @0],
-                               @"tempo":@[@100, @400, @700, @1000, @1300, @1600, @1900, @2200, @2500, @2800, @3100, @3400, @3700, @4000]
-                            },
-                          @"maryHadALittleLambArray",
-                           @{
-                               @"notes":@[@0, @0, @4, @4, @5, @5, @4, @3, @3, @2, @2, @1, @1, @0],
-                               @"tempo":@[@100, @400, @700, @1000, @1300, @1600, @1900, @2200, @2500, @2800, @3100, @3400, @3700, @4000]
-                            },
-                        ];
+    [self setUpArchitecture];
     
-    NSLog(@"%@", songlist[0]);
-    NSLog(@"%@", songlist[1]);
-    NSLog(@"%@", songlist[2]);
+    [self createColors];
 
 }
 
+- (void)setUpArchitecture
+{
+    colorDictionary1 = [@{
+                    @"yellow": @"image1",
+                    @"yellow": @"image2"
+                    }mutableCopy];
+    
+    faces = @{
+               @"yellow": @[@"face1", @"face2"],
+               @"red": @[@"face1", @"face2"],
+               };
+   //////////////////////////////////////////////////////////////
+   //////////////////////////////////////////////////////////////
+    
+//NSDictionary * face = colors[0][@"faces"][1][@"image"];
+    
+   colors = @[                          //array with nine dictionaries
+             @{                         //key is always a string
+                @"color": @"yellow",
+                @"image": @"colors0",
+                @"faces": @[
+                           @{
+                               @"emotion": @"happy",
+                               @"image" : @"yellow0"
+                            },
+                           @{
+                               @"emotion": @"sad",
+                               @"image" : @"yellow1"
+                               },
+                           ]
+                },
+             @{
+                 @"color": @"red",
+                 @"image": @"colors0",
+                 },
+             @{
+                 @"color": @"blue",
+                 @"image": @"colors1",
+                 },
+             @{
+                 @"color": @"green",
+                 @"image": @"colors2",
+                },
+             @{
+                 @"color": @"orange",
+                 @"image": @"colors3",
+                 },
+             @{
+                 @"color": @"purple",
+                 @"image": @"colors4",
+                 },
+             @{  @"color": @"pink",
+                 @"image": @"colors5",
+                 },
+             @{
+                 @"color": @"cyan",
+                 @"image": @"colors6",
+                 },
+             @{
+                 @"color": @"teal",
+                 @"image": @"colors7",
+                 }
+             ];
+    
+    colorDictionary2 = @{
+                        @"yellow": @{
+                             @"image": @"colors0",
+                             @"faces": @[
+                                     @{
+                                         @"emotion": @"happy",
+                                         @"image" : @"yellow0"
+                                         },
+                                     @{
+                                         @"emotion": @"happy",
+                                         @"image" : @"yellow0"
+                                         },
+                                 ]
+                         },
+                        @"yellow": @{
+                                @"image": @"colors0",
+                                @"faces": @[
+                                        @{
+                                            @"emotion": @"happy",
+                                            @"image" : @"yellow0"
+                                            },
+                                        @{
+                                            @"emotion": @"happy",
+                                            @"image" : @"yellow0"
+                                            },
+                                        ]
+                                }
+                        };
+}
 
+-(void)createColors
+{
+    NSString * yellowKey = [colorDictionary2 allKeys][0];
+    NSDictionary * yellowColor = colorDictionary2[yellowKey];
+    
+    for (NSString * key in [colorDictionary2 allKeys])
+    {
+        NSDictionary * color = colorDictionary2[key];
+        
+        NSLog(@"%@", color);
+    }
+    
+    
+    for (NSDictionary * color in colors) {
+        NSString * imageFileName = color[@"image"];
+ //       imageFileName = [color objectForKey:@"image"]; //slow way
 
+        NSLog(@"image file: %@", imageFileName);
+        
+        [self createFacesWithColorDictionary:color];
+        
+//        UIImage * colorImage = [UIImage imageNamed:imageFileName];
+    }
+    
+    NSDictionary * face = colors[0][@"faces"][1][@"image"];
+    NSLog(@"face: %@", face);
+}
+
+-(void)createFacesWithColor:(NSString * )color
+{
+    NSArray * faces = colorDictionary2[color][@"faces"];
+    
+    for (NSDictionary * face in faces) {
+        NSLog(@"%@", face);
+    }
+}
+
+-(void)createFacesWithColorDictionary:(NSDictionary * )color
+{
+    NSArray * faces = color[@"faces"];
+    for (NSDictionary * face in faces) {
+        NSLog(@"%@", face);
+    }
+    
+}
     
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
